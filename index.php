59598714c6de0ec29913a94a07b8fb9a84555594
AdamMcquiff <bg72db@student.sunderland.ac.uk>
2016-04-02 21:14:02 +0100

    Added files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset=utf-8>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Dual Sliding Hover Effect CSS3</title>
+	
+	<link rel="stylesheet" media="all" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body class="grid">
+	<main class="row">
+		<noscript>We strongly recommend enabling JavaScript for this website.</noscript>
+	
+		<section class="c12" id="">
+			<h1>Dual Sliding Hover Effect CSS3</h1>
+			
+			<div class="c4" style="height: 400px; background: url('http://images.freeimages.com/images/previews/bdc/national-theater-1451776.jpg'); background-size: cover;">
+				<div class="overlay-second">
+				
+				</div>
+				
+				<div class="overlay-first">
+					<p>Ancient Buildings</p>
+				</div>
+			</div>
+			
+			<div class="c4" style="height: 400px; background: url('http://images.freeimages.com/images/previews/6f7/national-theater-1451781.jpg'); background-size: cover;">
+				<div class="overlay-second">
+				
+				</div>
+				
+				<div class="overlay-first">
+					<p>Culture Icons</p>
+				</div>
+			</div>
+			
+			<div class="c4" style="height: 400px; background: url('http://images.freeimages.com/images/previews/c00/blaze-1308659.jpg'); background-size: cover;">
+				<div class="overlay-second">
+				
+				</div>
+				
+				<div class="overlay-first">
+					<p>Art Galleries</p>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<footer class="row">
+		<p>Created by <a href="http://www.adammckenna.co.uk">Adam McKenna</a></p>
+	</footer>
+</body>
+
+</html>
